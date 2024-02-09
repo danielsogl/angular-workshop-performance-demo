170ce05f3bc62609222440992026f808af9594f7
Daniel Sogl <me@danielsogl.de>
2024-02-09 11:13:11 +0100

    add network preloading demo

--- src/app/utils/custom-preloading.ts
+++ src/app/utils/custom-preloading.ts
@@ -8,3 +8,17 @@ export class CustomPreloadingStrategy implements PreloadingStrategy {
     return route.data && route.data['preload'] ? load() : of(null);
   }
 }
+
+
+
+@Injectable({ providedIn: "root" })
+export class CustomNetworkPreloadingStrategy implements PreloadingStrategy {
+  preload(_route: Route, load: () => Observable<unknown>): Observable<unknown> {
+    const connection = (navigator as any).connection;
+    if (connection.saveData || connection.effectiveType.includes('2g')) {
+      return of(null);
+    } else {
+      return load();
+    }
+  }
+}
